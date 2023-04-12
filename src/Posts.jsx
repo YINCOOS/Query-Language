c6fd0e2ca4b38c3fd581a8857e021dcc9f37fc03
Samson Osiomwan <81101034+samsonosiomwan@users.noreply.github.com>
2023-04-12 15:09:11 +0100

    trying some changes

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,21 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useQuery, useQueryClient } from "react-query";
 
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
+  const queryClient = useQueryClient();
+
+  useEffect(()=>{
+    if (currentPage < maxPostPage) {
+    const nextPage = currentPage + 1;
+    queryClient.prefetchQuery(["posts", nextPage], () => fetchPosts(nextPage));}
+  }, [currentPage, queryClient])
+
   // replace with useQuery
-  const data = [];
+  const { data, isError, error, isLoading } = useQuery(
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
+    { staleTime: 2000 }
+  );
+  if (isLoading) return <h3>Loading...</h3>;
+  if (isError)
+    return (
+      <>
+        <h3>Oops, something went wrong</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
 
   return (
     <>
@@ -31,11 +52,19 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button 
+        disabled={currentPage <= 1}
+        onClick={() => {
+          setCurrentPage((previousValue) => previousValue - 1);
+        }}>
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button 
+        disabled={currentPage >= maxPostPage} 
+        onClick={() => {
+          setCurrentPage((previousValue) => previousValue + 1);
+        }}>
           Next page
         </button>
       </div>
@@ -43,4 +72,14 @@ export function Posts() {
       {selectedPost && <PostDetail post={selectedPost} />}
     </>
   );
+}
+
+function computeClossetToZero(ts: number[]) {
+  let closest = ts[0];
+  for (let i = 1; i < ts.length; i++) {
+    if (Math.abs(ts[i]) < Math.abs(closest)) {
+      closest = ts[i];
+    }
+  }
+  return closest;
 }
